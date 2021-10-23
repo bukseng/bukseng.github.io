57eb80ac5fbb6a4038b0af18687593ed0b27a6fe
bukseng <glennabalos.mark@gmail.com>
2021-10-23 13:40:41 +0800

    remove global variables

--- random_chooser.js
+++ random_chooser.js
@@ -7,21 +7,21 @@ function choose(){
 
 
 function shuffle_and_pick(list){
-	n = list.length
-	iter_cnt = Math.floor((Math.random() * n));
+	let n = list.length
+	let iter_cnt = Math.floor((Math.random() * n));
 	
 	//shuffle
 	for (let i = 0; i <= iter_cnt; i++){
-		rand_1 = Math.floor((Math.random() * n));
-		rand_2 = Math.floor((Math.random() * n));
+		let rand_1 = Math.floor((Math.random() * n));
+		let rand_2 = Math.floor((Math.random() * n));
 
-		tmp = list[rand_1]
+		let tmp = list[rand_1]
 		list[rand_1] = list[rand_2]
 		list[rand_2] = tmp
 	}
 	
 	//pick
-	chosen = 0
+	let chosen = 0
 	for (let i = 0; i <= iter_cnt; i++){
 		chosen = Math.floor((Math.random() * n));
 	}
